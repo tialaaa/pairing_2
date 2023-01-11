06c4f85341575f8c27a853e600cd475a22969b96
Tiala Young <tialayoung@gmail.com>
2023-01-11 15:58:26 -0700

    Add final cleanup

--- friends.js
+++ friends.js
@@ -1,15 +1,12 @@
-var name = "Julie"
+var name = "Bob"
 
-
-
-var goodFriend = "Julie"
-
+var goodFriend = "Jim"
 
 
 if (name === "Sarah") {
     console.log("My name is Sarah")
 } else if (goodFriend === "Julie") {
     console.log("My name is Julie")
-} else if (name !== "Sarah") {
+} else {
     console.log("Everyone else")
 }
